adad6d4f99970b3c04587f36d8aca602a0d7f026
charitraa <stharabi9862187405@gmail.com>
2024-08-12 02:19:42 +0545

    create a gitinore and update the code

--- api/index.js
+++ api/index.js
@@ -1,34 +1,44 @@
 import express from 'express';
-import connectDB from './config/db.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+dotenv.config();
 
-const port = 5000;
-connectDB();
+mongoose
+  .connect("mongodb://localhost:27017/real_estate")
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
