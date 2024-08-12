d2d088c43a8490da21ee311cf14593eb30983e7e
charitraa <stharabi9862187405@gmail.com>
2024-08-12 17:14:22 +0545

    sloving props error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,6 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-    console.log('JWT_SECRET:', process.env.JWT_SECRET);
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || 'default_secret_key');
     const { password: pass, ...rest } = validUser._doc;
     res
